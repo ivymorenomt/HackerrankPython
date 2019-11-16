63175ebc5dc43d6d51e02cc064bb71db7bc3b752
Marjorie Ivy <marjorie_moreno@hotmail.com>
2019-11-15 21:51:00 -0600

    What's Your Name?

---
+++ Strings/PrintName.py
@@ -0,0 +1,6 @@
+def print_full_name(a, b):
+    print('Hello {0} {1}! You just delved into python.'.format(a,b))
+
+first = input('Enter first name: ')
+last = input('Enter last name: ')
+print_full_name(first, last)
