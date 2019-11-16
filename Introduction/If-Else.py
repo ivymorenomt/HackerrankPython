f27a4e176d3df6dbe8445edf195b961bf4cf50c3
Marjorie Ivy <marjorie_moreno@hotmail.com>
2019-11-15 21:17:17 -0600

    if-else

---
+++ Introduction/If-Else.py
@@ -0,0 +1,16 @@
+#Task
+# Given an integer, , perform the following conditional actions:
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+n = int(input('Enter a number: ').strip())
+
+check = {True : "Not Weird", False : "Weird"}
+
+print(check[
+		  n % 2 == 0 and (
+				  n in range(2, 6) or
+				  n > 20)
+		  ])
