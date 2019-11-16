269a2fec5992ddbca834955708a1c6508dfdeb74
Marjorie Ivy <marjorie_moreno@hotmail.com>
2019-11-15 21:30:58 -0600

    Division

---
+++ Introduction/PythonDivision.py
@@ -0,0 +1,9 @@
+#Read two integers and print two lines. The first line should contain integer division,  // . The second line should contain float division,  / .
+# You don't need to perform any rounding or formatting operations.
+
+a = int(input())
+b = int(input())
+d = a / b
+c = a / b
+print(int(d))
+print(float(c))
