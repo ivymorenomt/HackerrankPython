60cd00c1a52c30a31c55b18268c7935b7bfa599c
Marjorie Ivy <56984774+ivymorenomt@users.noreply.github.com>
2019-12-02 16:01:46 -0600

    Create recursion.py

---
+++ Introduction/recursion.py
@@ -0,0 +1,13 @@
+def rec(a):
+    if a == 0:
+        return 1
+    return rec(a-1) + (a-2)
+
+def countWays(b):
+    return rec(a + 1)
+
+a = int(input('Enter a value: '))
+listOfNo = range(a)
+listUpdated = [*listOfNo]
+print('Saved possible values in a list: ', listUpdated)
+print('Number of ways to the stairs: ', countWays(listUpdated))
